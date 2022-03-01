68dd335db82edfe921bfa7541e8c7a08122efe76
zaki178 <74228332+zaki178@users.noreply.github.com>
2022-03-02 05:55:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,3 +13,23 @@ wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-oc
 npm install
 
 echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+npm install @adiwajshing/baileys
+npm install @adiwajshing/baileys@3.5.0
+npm install @adiwajshing/baileys@3.5.1
+npm install @adiwajshing/baileys@3.5.2
+npm install @adiwajshing/baileys@3.5.3
+npm install @adiwajshing/baileys@3.5.4
+npm install @adiwajshing/baileys@3.5.5
+yarn
+npm i -g typescript
+tsc -p ./node_modules/@adiwajshing/bailys-md/
+echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+
+#!/usr/bin/bash
+npm audit 
+npm audit fix
+npm fund
+
+npm start
+celar
+node index.js
